67ca9be3acd4683ee950b43db6b13583a6ecb990
Albert <albert.koch1973@gmail.com>
2020-10-11 17:07:09 +0500

    add icons

--- .scripts/eyesaver/notify.sh
+++ .scripts/eyesaver/notify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-notify-send --icon=~/Pictures/icons/eye.png "Прошло 20 минут" "Время сделать перерыв"
+notify-send --icon=~/.icons/eye_normal.png "Прошло 20 минут" "Пора сделать перерыв"
