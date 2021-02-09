1fe25543bc24710cd9088133b6f01fc01e1e8be9
Albert <albert.fz@yandex.ru>
2021-02-09 20:21:37 +0500

    auto

--- .scripts/network/status.sh
+++ .scripts/network/status.sh
@@ -11,31 +11,35 @@ state=$(echo $line | cut -d: -f3)
 last_state=$(cat ~/.cache/network/$1/last_state)
 connection=$(echo $line | cut -d: -f4)
 
+
+[ -d ~/.cache/network/$1 ] || mkdir -p ~/.cache/network/$1
+[ -e ~/.cache/network/$1/last_state ] || echo "disconnected" > ~/.cache/network/$1/last_state
+
 msgId="5050"
 bgcolor=$(getcolor background)
 
 case $state in
     connected*)
         color=$(getcolor color2)
-        [ "$state" = "$last_state" ] || (dunstify "$device" "Подключен к $connection" \
+        [ "$state" != "$last_state" ] && (dunstify "$device" "Подключен к $connection" \
                                                   --appname=${type^^} \
                                                   --icon=~/.icons/i/${type}_green.png \
                                                   --replace=$msgId \
                                                   --hints=string:frcolor:$color \
                                                   --hints=string:fgcolor:$color \
                                                   --hints=string:bgcolor:$bgcolor
-                                         echo $state > ~/.cache/network/$1/last_state)
+                                          echo $state > ~/.cache/network/$1/last_state)
         ;;
     connecting*)
         color=$(getcolor color3)
-        [ "$state" = "$last_state" ] || (dunstify "$device" "Подключаюсь к $connection" \
+        [ "$state" != "$last_state" ] && (dunstify "$device" "Подключаюсь к $connection" \
                                                   --appname=${type^^} \
                                                   --icon=~/.icons/i/${type}_yellow.png \
                                                   --replace=$msgId \
                                                   --hints=string:frcolor:$color \
                                                   --hints=string:fgcolor:$color \
                                                   --hints=string:bgcolor:$bgcolor
-                                         echo $state > ~/.cache/network/$1/last_state)
+                                           echo $state > ~/.cache/network/$1/last_state)
         ;;
     disconnected*)
         color=$(getcolor color8)
@@ -51,7 +55,7 @@ case $state in
         ;;
     unavailable*)
         color=$(getcolor color1)
-        [ "$state" = "$last_state" ] || (dunstify "$device" "Устройство недоступно" \
+        [ "$state" != "$last_state" ] && (dunstify "$device" "Устройство недоступно" \
                                                   --appname=${type^^} \
                                                   --icon=~/.icons/i/${type}_critical.png \
                                                   --replace=$msgId \
--- .scripts/sxivweb/view.sh
+++ .scripts/sxivweb/view.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+[ -d ~/.cache/sxivweb ] || mkdir -p ~/.cache/sxivweb
+
 name=$(date +%s)
 
-curl -s "$1" > .cache/sxivweb/$name
-sxiv .cache/sxivweb/$name
-rm -rf .cache/sxivweb/$name
+curl -s "$1" > ~/.cache/sxivweb/$name
+sxiv ~/.cache/sxivweb/$name
+rm -rf ~/.cache/sxivweb/$name
