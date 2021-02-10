30ef8c8bed5eb17aa6045d0ef84a337714de1610
Albert <albert.fz@yandex.ru>
2021-02-10 16:18:24 +0500

    auto

--- .zsh/plug.sh
+++ .zsh/plug.sh
@@ -37,11 +37,35 @@ plug_install() {
 }
 
 plug_update() { 
+    declare -A downloads
     declare -A fetched
     declare -A merged
+    
     echo -e "\033[0;32mUpdating plugins...\033[0m"
-    while read -r line; do
+    
+    while read -r line; do # install plugins
         repo="${line##*/}"
+        [ -d "${SCRIPTPATH}/plug/${repo}" ] && continue
+        ( git -C "${SCRIPTPATH}/plug" clone "$line" > /dev/null 2>>"${LOGS}/${repo}" ) &
+        downloads[$!]="$repo"   
+    done < "${SCRIPTPATH}/list"
+    
+    for d in "${!downloads[@]}"; do
+        code=0
+        wait "$d" || code=$?
+        if [ "$code" != "0" ]; then
+            echo -e "${downloads[$d]}: \033[0;31mError[$code] occurred when cloned\033[0m"
+        else
+            echo -e "\033[0;32m${downloads[$d]}: installed!\033[0m"
+        fi
+    done
+    
+
+    while read -r line; do # update plugins
+        repo="${line##*/}"
+        case "${downloads[@]}" in
+            *$repo*) continue;;
+        esac
         ( git -C "${SCRIPTPATH}/plug/${repo}" fetch > /dev/null 2>>"${LOGS}/${repo}") &
         fetched[$!]="$repo"   
     done < "${SCRIPTPATH}/list"
@@ -54,14 +78,16 @@ plug_update() {
             (git -C "${SCRIPTPATH}/plug/${fetched[$r]}" merge origin/master > /dev/null 2>>"${LOGS}/${repo}" ) &
             merged[$!]="${fetched[$r]}"
         else
-            echo -e "\033[0;33m${fetched[$r]} is already up to date!\033[0m"
+            echo -e "\033[0;33m${fetched[$r]}: already up to date!\033[0m"
         fi
     done
 
     for r in "${!merged[@]}"; do
         wait "$r"
-        echo -e "\033[0;32m${merged[$r]} updated!\033[0m"
+        echo -e "\033[0;32m${merged[$r]}: updated!\033[0m"
     done
+
+    plug_clean
 
 }
 
@@ -70,7 +96,7 @@ plug_clean() {
         repo="${plug##*/}"
         if [ ! $(grep $repo "${SCRIPTPATH}/list")  ]; then
             rm -rf $plug
-            echo -e "\033[0;31m${repo} was deleted\033[0m"
+            echo -e "\033[0;31m${repo}: deleted\033[0m"
         fi
     done
 }
