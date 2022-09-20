24544ff8cafa68c657f0bf9ec6fcd049d8655525
Albert Fazlyev <albert.fz@yandex.ru>
2022-09-20 18:37:57 +0500

    auto

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -7,7 +7,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     }
 )
 
-local servers = {'pylsp', 'clangd', 'html', 'cssls', 'phpactor'}
+local servers = {'pylsp', 'clangd', 'html', 'cssls', 'phpactor', 'csharp_ls'}
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         on_attach = on_attach,
