52c6a177049e51d0c491044b5fc56739866e20d8
Albert Fazlyev <albert.fz@yandex.ru>
2023-12-14 19:01:52 +0500

    auto

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -45,12 +45,6 @@ return {
         end,
     },
     {
-	    "L3MON4D3/LuaSnip",
-	    version = "2.*",
-	    build = "make install_jsregexp",
-        event = "InsertEnter",
-    },
-    {
         "airblade/vim-gitgutter",
 	    event = { "BufReadPost", "BufNewFile" }
     },
@@ -60,26 +54,27 @@ return {
 	    cmd = "Telescope",
         dependencies = {
             "nvim-lua/plenary.nvim",
-	        {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                config = function()
-                    telescope = require("telescope")
-		            telescope.load_extension("fzf")
-                    telescope.setup {
-                        extensions = {
-                            fzf = {
-                                fuzzy = true,
-                                override_generic_sorter = true,
-                                override_file_sorter = true,
-                                case_mode = "smart_case",
-                            }
-                        }
+            "nvim-telescope/telescope-fzf-native.nvim"
+        },
+        config = function()
+            telescope = require("telescope")
+	        telescope.load_extension("fzf")
+            telescope.setup {
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
                     }
-                end,
+                }
             }
-        }
-
+        end,
+    },
+	{
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+	    cmd = "Telescope",
     },
     {
         "github/copilot.vim",
--- .config/nvim/lua/plugins/nvimcmp.lua
+++ .config/nvim/lua/plugins/nvimcmp.lua
@@ -12,6 +12,7 @@ return {
         config = function()
             local cmp = require('cmp')
             cmp.setup({
+                preselect = cmp.PreselectMode.None,
                 snippet = {
                     expand = function(args)
                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
@@ -26,7 +27,7 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
